92dc0cc7850f8879020b1dcffe9492ca880a4f3c
Tristan Bowler <tristan.bowler@gmail.com>
2019-10-14 14:16:43 -0600

    Reimplement Camera Blur and Vignette effect

---
+++ Personal_Space/Assets/Scripts/VignetteControl.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Rendering.PostProcessing;
+
+public class VignetteControl : MonoBehaviour
+{
+
+
+    public GameObject player;
+    private float intensity;
+    private Vignette vignette = null;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        PostProcessVolume volume = this.GetComponent<PostProcessVolume>();
+        volume.profile.TryGetSettings(out vignette);
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        vignette.enabled.value = true;
+        float healthFraction = player.GetComponent<PlayerController>().health / 100;
+        vignette.intensity.value = 1 - healthFraction;
+    }
+}
