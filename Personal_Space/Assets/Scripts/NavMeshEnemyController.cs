ee2ad6e46f30b258db5b00c2d2c9b02b044b2083
Unknown <sridharan97@gmail.com>
2019-10-22 15:13:58 -0600

    Added NavMesh Scene, Enemy, and Obstacle
    Enemy follows player, points toward movement. Has a nose. Supes Cute.

---
+++ Personal_Space/Assets/Scripts/NavMeshEnemyController.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class NavMeshEnemyController : MonoBehaviour
+{
+    public float followSpeed;
+    public float moveBackSpeed;
+
+    private Vector3 startingPosition;
+    private Transform playerPosition;
+    private NavMeshAgent agent; 
+
+    void Start()
+    {
+
+        // Initialize variables
+        playerPosition = GameObject.FindGameObjectWithTag("Player").GetComponent<Transform>();
+        startingPosition = transform.position;
+        agent = GetComponent<NavMeshAgent>();
+    }
+
+    void Update()
+    {
+          agent.destination = playerPosition.position; 
+    }
+
+
+    //--------------------HELPER METHODS--------------------//
+
+    /**
+     *  Helper methods that moves the enemy to the player
+     **/
+    public void moveTowardsPlayer()
+    {
+        transform.position = Vector3.MoveTowards(transform.position, playerPosition.position, followSpeed * Time.deltaTime);
+    }
+
+    /**
+     *  Helper methods that moves the enemy to the thier starting location
+     **/
+    public void moveBackToStart()
+    {
+        transform.position = Vector3.MoveTowards(transform.position, startingPosition, moveBackSpeed * Time.deltaTime);
+    }
+    
+}
