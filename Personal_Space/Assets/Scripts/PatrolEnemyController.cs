0dab739445338a78b7422311c876543fcd11a32c
Unknown <sridharan97@gmail.com>
2019-10-22 22:09:48 -0600

    Added InstaKill enemy that also follows patrol path
    The path is set using an array of object transforms. Also, you have to move in the enemy zone for the instakill to take effect. Moving zones have also been implemented. I parented the zone to the player in SampleInstaKill Scene.

---
+++ Personal_Space/Assets/Scripts/PatrolEnemyController.cs
@@ -0,0 +1,61 @@
+﻿ using UnityEngine;
+    using UnityEngine.AI;
+    using System.Collections;
+
+
+    public class PatrolEnemyController : MonoBehaviour {
+
+        public Transform[] points;
+        private int destPoint = 0;
+        private NavMeshAgent agent;
+        private float agentSpeed;
+
+        private bool waiting = false;
+        void Start () {
+            agent = GetComponent<NavMeshAgent>();
+            agentSpeed = agent.speed;
+            // Disabling auto-braking allows for continuous movement
+            // between points (ie, the agent doesn't slow down as it
+            // approaches a destination point).
+            agent.autoBraking = false;
+
+            GotoNextPoint();
+        }
+
+
+        void GotoNextPoint() {
+            // Returns if no points have been set up
+            if (points.Length == 0)
+                return;
+
+            // Set the agent to go to the currently selected destination.
+            agent.destination = points[destPoint].position;
+
+            // Choose the next point in the array as the destination,
+            // cycling to the start if necessary.
+            destPoint = (destPoint + 1) % points.Length;
+        }
+
+
+        void Update () {
+            // Choose the next destination point when the agent gets
+            // close to the current one.
+            if (!agent.pathPending && agent.remainingDistance < 0.5f)
+                {
+                    if(!waiting)
+                    {
+                        agent.speed = 0;
+                        waiting = true;
+                        StartCoroutine(Loiter());
+                    }
+                }
+        }
+
+        private IEnumerator Loiter()
+        {
+             yield return new WaitForSeconds(4f);
+             agent.speed = agentSpeed;
+             GotoNextPoint();
+             waiting = false;
+        }
+    }
