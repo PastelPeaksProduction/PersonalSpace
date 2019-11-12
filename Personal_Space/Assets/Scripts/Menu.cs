9a6bd2bc59fe7bb59a95803b46be8aa5e2a2867f
fengfeng8867dd@hotmail.com <fengfeng8867dd@hotmail.com>
2019-11-12 05:38:02 -0700

    A better starting menu, known bug: back -> control will not be place in the right place

--- Personal_Space/Assets/Scripts/Menu.cs
+++ Personal_Space/Assets/Scripts/Menu.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
@@ -7,18 +8,26 @@ public class Menu : MonoBehaviour
 {
     public GameObject Camera;
     public GameObject ControlPos;
+    public GameObject OrigMainPos;
     public GameObject BackMainPos;
+    public GameObject AwayMenuPos;
+    public GameObject MainMenuParent;
+    public GameObject InstrucParent;
+    public GameObject InstructionPos;
+    public GameObject InstructionPosExit;
+
     private bool ControlTrans = false;
     private bool BackTrans = false;
-
+    private bool HideMainTrans = false;
+    private bool ShowMainTrans;
 
     private void Start()
     {
-        
+
     }
     public void StartGame()
     {
-        SceneManager.LoadScene("SchoolDance");
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
     }
 
     public void QuitGame()
@@ -28,19 +37,38 @@ public class Menu : MonoBehaviour
 
     public void BackToMain()
     {
-        BackTrans = true;
+        ShowMainTrans = true;
+        // BackTrans = true;
     }
 
     public void ShowControls()
     {
+        HideMainTrans = true;
         ControlTrans = true;
     }
 
+
     private void FixedUpdate()
     {
+        if (HideMainTrans)
+        {
+            MainMenuParent.GetComponent<RectTransform>().rotation = Quaternion.RotateTowards(MainMenuParent.GetComponent<RectTransform>().rotation, AwayMenuPos.GetComponent<RectTransform>().rotation, 20 * Time.deltaTime);
+        }
+        if (ShowMainTrans)
+        {
+            HideMainTrans = false;
+
+            InstrucParent.transform.position = Vector3.MoveTowards(InstrucParent.transform.position, new Vector3(-122, 50, -251), 22 * Time.deltaTime);
+
+
+            MainMenuParent.GetComponent<RectTransform>().rotation = Quaternion.RotateTowards(MainMenuParent.GetComponent<RectTransform>().rotation, BackMainPos.GetComponent<RectTransform>().rotation, 13 * Time.deltaTime);
+            MoveCam(OrigMainPos, ref ShowMainTrans);
+        }
         if (ControlTrans)
         {
-            MoveCam(ControlPos,ref ControlTrans);
+            MoveCam(ControlPos, ref ControlTrans);
+            InstrucParent.transform.position = Vector3.MoveTowards(InstrucParent.transform.position, InstructionPos.transform.position, 20 * Time.deltaTime);
+
         }
         else if (BackTrans)
         {
@@ -56,5 +84,10 @@ public class Menu : MonoBehaviour
 
         Camera.transform.position = Vector3.MoveTowards(Camera.transform.position, moveTo.transform.position, 20 * Time.deltaTime);
         Camera.transform.rotation = Quaternion.RotateTowards(Camera.transform.rotation, moveTo.transform.rotation, 20 * Time.deltaTime);
+    }
+
+    private void EnsureInstr()
+    {
+
     }
 }
