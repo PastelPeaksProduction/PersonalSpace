4c8d59844a11638a48331e57f6805bec47be4e8a
fengfeng8867dd@hotmail.com <fengfeng8867dd@hotmail.com>
2019-11-12 03:37:46 -0700

    test start menu

---
+++ Personal_Space/Assets/Scripts/LogoSript.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LogoSript : MonoBehaviour
+{
+
+    private double popTime = 0.3f;
+    private double zoomTime = 0.3f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+       
+    }
+
+    private void FixedUpdate()
+    {
+        PopEffect();
+    }
+
+    private void PopEffect()
+    {
+        popTime -= Time.deltaTime;
+        if(popTime < 0)
+        {
+            zoomTime -= Time.deltaTime;
+            transform.localScale -= new Vector3(0.002f, 0.002f, 0.002f);
+            if(zoomTime < 0)
+            {
+                popTime = 1f;
+                zoomTime = 1f;
+            }
+        }
+        else
+        {
+            transform.localScale += new Vector3(0.002f, 0.002f, 0.002f);
+        }
+    }
+}
--- Personal_Space/Assets/Scripts/Menu.cs
+++ Personal_Space/Assets/Scripts/Menu.cs
@@ -5,6 +5,17 @@ using UnityEngine.SceneManagement;
 
 public class Menu : MonoBehaviour
 {
+    public GameObject Camera;
+    public GameObject ControlPos;
+    public GameObject BackMainPos;
+    private bool ControlTrans = false;
+    private bool BackTrans = false;
+
+
+    private void Start()
+    {
+        
+    }
     public void StartGame()
     {
         SceneManager.LoadScene("SchoolDance");
@@ -15,8 +26,35 @@ public class Menu : MonoBehaviour
         Application.Quit();
     }
 
+    public void BackToMain()
+    {
+        BackTrans = true;
+    }
+
     public void ShowControls()
     {
-        SceneManager.LoadScene("ControlsMenu");
+        ControlTrans = true;
+    }
+
+    private void FixedUpdate()
+    {
+        if (ControlTrans)
+        {
+            MoveCam(ControlPos,ref ControlTrans);
+        }
+        else if (BackTrans)
+        {
+            MoveCam(BackMainPos, ref BackTrans);
+
+        }
+    }
+
+    private void MoveCam(GameObject moveTo, ref bool transBool)
+    {
+        if (Camera.transform.position == moveTo.transform.position && Camera.transform.rotation == moveTo.transform.rotation)
+            transBool = false;
+
+        Camera.transform.position = Vector3.MoveTowards(Camera.transform.position, moveTo.transform.position, 20 * Time.deltaTime);
+        Camera.transform.rotation = Quaternion.RotateTowards(Camera.transform.rotation, moveTo.transform.rotation, 20 * Time.deltaTime);
     }
 }
