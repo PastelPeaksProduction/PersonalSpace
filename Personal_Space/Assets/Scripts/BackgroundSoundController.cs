eddeb0b5088d8c6f1dd84dccfe38f38bc8d2ddeb
Alex Wittmann <alexwittmann01@gmail.com>
2019-11-16 15:58:39 -0700

    Added soundScene with initial testing

--- Personal_Space/Assets/Scripts/BackgroundSoundController.cs
+++ Personal_Space/Assets/Scripts/BackgroundSoundController.cs
@@ -5,39 +5,111 @@ using UnityEngine;
 public class BackgroundSoundController : MonoBehaviour
 {
     public AudioSource backgroundSound;
+    public AudioSource stressSound;
+    public AudioSource calmSound;
+
+
+    public float backgroundNormal;
+    public float backgroundStop;
+    public float backgroundZone;
+
+    public float calmVolume;
+    public float calmStop;
+
+    public float stressVolume;
+    public float stressStop;
+
+    public float lowPassStop;
+    public float lowPassMoving;
+
     private bool isMoving;
+    private bool inSafe = false;
+    private bool inDanger = false;
+    private float backgroundVolume;
     void Start()
     {
+        backgroundVolume = backgroundNormal;
     }
 
     void Update()
     {
-        isMoving = GetComponentInParent<PlayerController>().isMoving; 
-        if (isMoving)
-        {
-            backgroundSound.volume = 1;
-        }
-        else
-        {
-            backgroundSound.volume = 0.4f;
-        }
-
+        isMoving = GetComponentInParent<PlayerController>().isMoving;
+        setVolume(isMoving);
     }
 
     private void OnTriggerEnter(Collider other)
     {
         if (other.gameObject.CompareTag("SafeZone"))
         {
-            backgroundSound.pitch = 0.5f;
+            backgroundVolume = backgroundZone;
+            inSafe = true;
+            calmSound.Play();
+            calmSound.volume = calmVolume;
+            if (inDanger)
+            {
+                stressSound.Stop();
+            }
         }
         else if(other.gameObject.CompareTag("DangerZone"))
         {
-            backgroundSound.pitch = 2f;
+            backgroundVolume = backgroundZone;
+            inDanger = true;
+            if (!inSafe)
+            {
+                stressSound.Play();
+                stressSound.volume = stressVolume;
+            }
         }
     }
 
     private void OnTriggerExit(Collider other)
     {
-        backgroundSound.pitch = 1f;
+        if (other.gameObject.CompareTag("SafeZone"))
+        {
+            calmSound.Stop();
+            inSafe = false;
+            if (inDanger)
+            {
+                stressSound.Play();
+                stressSound.volume = stressVolume;
+            }
+        }
+        else if (other.gameObject.CompareTag("DangerZone"))
+        {
+            stressSound.Stop();
+            inDanger = false;
+        }
+        if(!inDanger && !inSafe)
+        {
+            backgroundVolume = backgroundNormal;
+        }
+    }
+
+    /// <summary>
+    /// helper function that sets volume based on movement
+    /// </summary>
+    /// <param name="isMoving"></param>
+    private void setVolume(bool isMoving)
+    {
+        if (isMoving)
+        {
+            backgroundSound.volume = backgroundVolume;
+            backgroundSound.GetComponent<AudioLowPassFilter>().cutoffFrequency = lowPassMoving;
+            stressSound.volume = stressVolume;
+            stressSound.GetComponent<AudioLowPassFilter>().cutoffFrequency = lowPassMoving;
+            calmSound.volume = calmVolume;
+            calmSound.GetComponent<AudioLowPassFilter>().cutoffFrequency = lowPassMoving;
+
+        }
+        else
+        {
+            backgroundSound.volume = backgroundStop;
+            backgroundSound.GetComponent<AudioLowPassFilter>().cutoffFrequency = lowPassStop;
+            stressSound.volume = stressStop;
+            stressSound.GetComponent<AudioLowPassFilter>().cutoffFrequency = lowPassStop;
+            calmSound.volume = calmStop;
+            calmSound.GetComponent<AudioLowPassFilter>().cutoffFrequency = lowPassStop;
+        }
+
     }
 }
