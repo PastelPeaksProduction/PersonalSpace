5199732dc4b5aded5b96a57eb3572123f44259d3
Unknown <sridharan97@gmail.com>
2019-11-19 14:00:00 -0700

    Papa? Papa can u hear me? Mama is that you?
    A parent object and a new scene are in! The parent ParentalUnit prefab works like the puppy, but will wait til the player is in their zone to move.

---
+++ Personal_Space/Assets/Scripts/PatrolCalmController.cs
@@ -0,0 +1,89 @@
+﻿ using UnityEngine;
+    using UnityEngine.AI;
+    using System.Collections;
+
+
+    public class PatrolCalmController : MonoBehaviour {
+
+        public Transform[] points;
+        private int destPoint = 0;
+        private NavMeshAgent agent;
+        private float agentSpeed;
+
+        public float waitTime;
+        public GameObject zone;
+        private ZoneScript zoneControl;
+
+        private bool waiting = false;
+        private bool waitingForKid = false;
+        private PlayerController playerController;
+        void Start () 
+        {
+            agent = GetComponent<NavMeshAgent>();
+            agentSpeed = agent.speed;
+            agent.autoBraking = false;
+            zoneControl = zone.GetComponent<ZoneScript>();
+            playerController = GameObject.FindGameObjectWithTag("Player").GetComponent<PlayerController>();
+            GotoNextPoint();
+        }
+
+
+        void GotoNextPoint()
+        {
+            // Returns if no points have been set up
+            if (points.Length == 0)
+                return;
+
+            // Set the agent to go to the currently selected destination.
+            agent.destination = points[destPoint].position;
+
+            // Choose the next point in the array as the destination,
+            // cycling to the start if necessary.
+            destPoint = (destPoint + 1) % points.Length;
+        }
+
+
+        void Update () 
+        {
+            // Choose the next destination point when the agent gets
+            // close to the current one.
+            if (!agent.pathPending && agent.remainingDistance < 0.5f)
+            {
+                if(!waiting)
+                {
+                    agent.speed = 0;
+                    waiting = true;
+                }
+                    
+            }
+            if(!waiting)
+            {
+                if(playerController.isMoving || playerController.isBreathing)
+                {
+                    agent.speed = agentSpeed;
+                }
+                else
+                {
+                    agent.speed = 0;
+                }
+            }
+            else
+            {
+                agent.speed = 0;
+                if(zoneControl.getPlayerInZone() && !waitingForKid)
+                {
+                    waitingForKid = true;
+                    StartCoroutine(Loiter());
+                }
+            }
+        }
+
+        private IEnumerator Loiter()
+        {
+            yield return new WaitForSeconds(waitTime);
+            agent.speed = agentSpeed;
+            GotoNextPoint();
+            waiting = false;
+            waitingForKid = false;
+        }
+    }
