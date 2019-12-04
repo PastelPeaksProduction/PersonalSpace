f757abedaacc1da8d6afec954e6c7217181075d5
kenmingwang <35542088+kenmingwang@users.noreply.github.com>
2019-12-03 19:31:30 -0700

    Level select, controls menu

---
+++ Personal_Space/Assets/Scripts/UI/GrocerySelect.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+public class GrocerySelect : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnMouseDown()
+    {
+        switch(gameObject.name){
+            case "01Grocery": SceneManager.LoadScene("01GroceryStore");break;
+            case "02SchoolDance": SceneManager.LoadScene("02SchoolDance"); break;
+            case "03HouseParty": SceneManager.LoadScene("03HouseParty"); break;
+            case "04Office": SceneManager.LoadScene("04WorkParty"); break;
+        }
+
+    }
+}
