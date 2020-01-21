457e37bc01feb7e32d1a28db9524a4c67ad8120b
kenmingwang <35542088+kenmingwang@users.noreply.github.com>
2020-01-21 09:22:01 -0700

    Added Arrow indicator demo

---
+++ Personal_Space/Assets/Scripts/UI/ArrowIndicator.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UIUtil;
+using UnityEngine;
+
+public class ArrowIndicator : MonoBehaviour
+{
+    public GameObject ArrowPrefab;
+    private GameObject ArrowObj;
+    private RectTransform ArrowTrans;
+    private UIWorldSpaceUti CanvasUtil = new UIWorldSpaceUti();
+    private GameObject Canvas;
+    private GameObject Player;
+    public GameObject Objective;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Canvas = GameObject.Find("EnemyBubbleCanvas");
+        Player = GameObject.Find("Player");
+        ArrowObj = Instantiate(ArrowPrefab);
+        ArrowObj.transform.SetParent(Canvas.transform);
+        ArrowTrans = ArrowObj.GetComponent<RectTransform>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        ArrowTrans.position = gameObject.transform.position + new Vector3(5,-4.5f);
+        ArrowTrans.LookAt(Objective.transform);
+        ArrowTrans.Rotate(new Vector3(90, -90, 0));
+    }
+}
