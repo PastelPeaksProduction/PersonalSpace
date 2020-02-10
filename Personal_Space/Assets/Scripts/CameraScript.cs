93802e85f4dce1d44782e487f7ec441411a5cfd8
Alex Wittmann <alexwittmann01@gmail.com>
2020-02-09 19:58:26 -0700

    New Camera and Stuff

--- Personal_Space/Assets/Scripts/CameraScript.cs
+++ Personal_Space/Assets/Scripts/CameraScript.cs
@@ -23,9 +23,12 @@ public class CameraScript : MonoBehaviour
     public float smoothing = 1.0f;
     private bool isPaused;
 
+    private Vector3 offset;
+
     void Start()
     {
         Player = GameObject.Find("Player");
+        offset = targetPositionTPP.transform.position;
         perspective = false; // False is TPP, True is FPP
         if (Application.platform == RuntimePlatform.WindowsEditor || Application.platform == RuntimePlatform.WindowsPlayer)
         {
@@ -57,12 +60,26 @@ public class CameraScript : MonoBehaviour
         }
         else
         {
-            ThirdPersonPerspective();
+            //ThirdPersonPerspective();
+            NewThirdPersonPerspective();
         }
 
 
     }
 
+    private void NewThirdPersonPerspective()
+    {
+        if (Input.GetAxisRaw(horizontalController) != 0)
+        {
+            offset = Quaternion.AngleAxis(Input.GetAxisRaw(horizontalController) * speed, Vector3.up) * offset;
+        }
+        else
+        {
+            offset = Quaternion.AngleAxis(Input.GetAxisRaw("Mouse X") * speed, Vector3.up) * offset;
+        }
+        transform.position = Player.transform.position + offset;
+        transform.LookAt(Player.transform.position);
+    }
     private void ThirdPersonPerspective()
     {
         if (!isPaused)
@@ -92,7 +109,7 @@ public class CameraScript : MonoBehaviour
             sommthV.x = Mathf.Lerp(sommthV.x, md.x, 1f / smoothing);
             mouseLook += sommthV;
 
-            Player.transform.localRotation = Quaternion.AngleAxis(mouseLook.x, Player.transform.up);
+            //Player.transform.localRotation = Quaternion.AngleAxis(mouseLook.x, Player.transform.up);
 
             transform.position = Vector3.MoveTowards(transform.position, targetPositionTPP.transform.position, CameraSpeed * Time.deltaTime);
 
