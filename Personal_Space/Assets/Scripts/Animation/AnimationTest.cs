4a459cf2c700d857b76a77b378f9ff42eb41e7a0
Tristan Bowler <tristan.bowler@gmail.com>
2020-02-10 16:51:36 -0700

    Animations?

---
+++ Personal_Space/Assets/Scripts/Animation/AnimationTest.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AnimationTest : MonoBehaviour
+{
+    Animator anim;
+    public PlayerController1 controller;
+    bool walking = false;
+    // Start is called before the first frame update
+    void Start()
+    {
+        anim = this.GetComponent<Animator>();
+        anim.SetBool("isWalking", false);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (controller.isMoving != walking)
+        {
+            walking = controller.isMoving;
+            anim.SetBool("isWalking", walking);
+        }
+        
+    }
+}
