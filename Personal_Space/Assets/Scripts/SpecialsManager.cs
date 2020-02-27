b6e0047a436fc2835995876bfa40c5d1ff4f9020
tristanbowler <tristan.bowler@gmail.com>
2020-02-27 08:37:23 -0700

    test2

--- Personal_Space/Assets/Scripts/SpecialsManager.cs
+++ Personal_Space/Assets/Scripts/SpecialsManager.cs
@@ -20,6 +20,7 @@ public class SpecialsManager : MonoBehaviour
     public float shoutRadius = 40f;
     public float shoutUpwardsForce = 10f;
 
+
     // Start is called before the first frame update
     void Start()
     {
