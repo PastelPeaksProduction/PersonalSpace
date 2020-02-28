b6003fb17dff8da65ff11c895651034dc2efc917
Sridharan97 <sridharan97@gmail.com>
2020-02-27 23:06:56 -0700

    Randomizing script for accessories
    Uploaded a script that'll randomly assign accessories based off of a given list and specific spawn points. Prefabbed character is Capsule Testing Accessories. Script is SpawnAccessory.

---
+++ Personal_Space/Assets/Scripts/SpawnAccessory.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnAccessory : MonoBehaviour
+{
+    public GameObject[] options;
+    private int optionsLength = 0;
+    // Start is called before the first frame update
+    void Start()
+    {
+        if(options != null)
+        {
+            if(options.Length > 1)
+            {
+                optionsLength = options.Length;
+                
+                int randomAccInd =  Random.Range(0,optionsLength);
+                //Debug.Log(""+randomAccInd);
+                Instantiate(options[randomAccInd], transform.position, transform.rotation);
+            }
+            else
+            {
+                Instantiate(options[0], transform.position, transform.rotation);
+            } 
+        }    
+    }
+}
