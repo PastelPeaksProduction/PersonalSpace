2be08e71b6ac793c7431d51150b502f93021f5f1
Tristan Bowler <tristan.bowler@gmail.com>
2020-04-13 15:42:39 -0600

    Some changes for randomized shapes

---
+++ Personal_Space/Assets/Scripts/RandomizeShapes.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RandomizeShapes : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public Mesh mesh;
+    void Start()
+    {
+        Mesh[] library = GameObject.FindGameObjectWithTag("ShapeLibrary").GetComponent<RandomShapesLibrary>().shapes;
+        int random = Random.Range(0, library.Length);
+        mesh = library[random];
+        this.GetComponent<MeshFilter>().mesh = mesh;
+        this.transform.localScale = new Vector3(1, 1, 1);
+    }
+
+    // Update is called once per frame
+    
+}
