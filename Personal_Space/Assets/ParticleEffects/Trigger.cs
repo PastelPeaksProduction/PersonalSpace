b66a4f6dd460a3d18e5cea8199c57e3b2a18fb85
Tristan Bowler <tristan.bowler@gmail.com>
2020-04-15 23:50:46 -0600

    dang guys these particle effects are sick

---
+++ Personal_Space/Assets/ParticleEffects/Trigger.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Trigger : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public ParticleSystem[] particles;
+
+    //should be between 0 and length(particles) - 1
+    public int test; 
+    void Start()
+    {
+        //This just loops through all of them and stops them to start. 
+        foreach( ParticleSystem p in particles)
+        {
+            p.Stop();
+        }
+
+        //This takes the value in the test field and turns on just that effect. 
+        particles[test].Play();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
