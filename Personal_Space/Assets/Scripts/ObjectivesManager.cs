870982893283c64868f6dc20a6650ac35e0b0c5c
Tristan Bowler <tristan.bowler@gmail.com>
2020-04-22 18:46:54 -0600

    Fix objective issue in work party

--- Personal_Space/Assets/Scripts/ObjectivesManager.cs
+++ Personal_Space/Assets/Scripts/ObjectivesManager.cs
@@ -30,53 +30,7 @@ public class ObjectivesManager : MonoBehaviour
 
     public NewObjective[] Objectives;
 
-    /* Object to trigger Objective */
-    public string gameStartObjectiveDescription;
-    public Sprite gameStartEmoji;
-
-    public GameObject firstObject_1;
-    public string firstDescription;
-    public Sprite firstEmoji;
-
-    public GameObject secondObject_2;
-    public string secondDescription;
-    public Sprite secondEmoji;
-
-    public GameObject thirdObject_3;
-    public string thirdDescription;
-    public Sprite thirdEmoji;
-
-    public GameObject fourthObject_4;
-    public string fourthDescription;
-    public Sprite fourthEmoji;
-
-    public GameObject fifthObject_5;
-    public string fifthDescription;
-    public Sprite fifthEmoji;
-
-    public GameObject sixthObject_6;
-    public string sixthDescription;
-    public Sprite sixthEmoji;
-
-    public GameObject seventhObject_7;
-    public string seventhDescription;
-    public Sprite seventhEmoji;
-
-    public GameObject eightObject_8;
-    public string eightDescription;
-    public Sprite eightEmoji;
-
-    public GameObject ninthObject_9;
-    public string ninthDescription;
-    public Sprite ninthEmoji;
-
-    public GameObject tenthObject_10;
-    public string tenthDescription;
-    public Sprite tenthEmoji;
-
-    public GameObject pauseDialogText;
-    public float reminderTime;
-    public GameObject ObjMarker;
+    
 
     //private ObjectiveMarker ObjMarkerSingle;
     //private List<Objective> Objectives;
@@ -99,80 +53,23 @@ public class ObjectivesManager : MonoBehaviour
         _phoneUI = GameObject.Find("PopUpPhone").GetComponent<PhoneUI>();
         dataPost = GameObject.FindGameObjectWithTag("Data").GetComponent<DrivePost>();
 
+        Objectives[0].objectiveObject.SetActive(true);
         for (int i = 1; i < Objectives.Length; i++)
         {
             if (Objectives[i].objectiveObject != null)
             {
-                Objectives[i].objectiveObject.SetActive(false);
+                //Debug.Log("Turning off " + Objectives[i].objectiveObject.name);
+                if (Objectives[0].objectiveObject != Objectives[i].objectiveObject)
+                {
+                    Objectives[i].objectiveObject.SetActive(false);
+                }
+                
             }
         }
 
-        /*Objectives = new List<Objective>();
-        //  CHRIS CODE
-        //  Set each objective as false when game initializes, and set each active as the previous one is triggered
-        //  Added to intantiating of each objective as well as one line down in TRIGGERED
-        if (firstObject_1 != null)
-        {
-            Objectives.Add(new Objective(firstObject_1, firstDescription,firstEmoji));
-            firstObject_1.SetActive(true); //
-
-        }
-        if (secondObject_2 != null)
-        {
-            Objectives.Add(new Objective(secondObject_2, secondDescription, secondEmoji));
-            secondObject_2.SetActive(false);    //
-        }
-        if (thirdObject_3 != null)
-        {
-            Objectives.Add(new Objective(thirdObject_3, thirdDescription, thirdEmoji));
-            thirdObject_3.SetActive(false); //
-        }
-        if (fourthObject_4 != null)
-        {
-            Objectives.Add(new Objective(fourthObject_4, fourthDescription, fourthEmoji));
-            fourthObject_4.SetActive(false);    //
-        }
-        if (fifthObject_5 != null)
-        {
-            Objectives.Add(new Objective(fifthObject_5, fifthDescription, fifthEmoji));
-            fifthObject_5.SetActive(false); //
-        }
-
-        if (sixthObject_6 != null)
-        {
-            Objectives.Add(new Objective(sixthObject_6, sixthDescription, sixthEmoji));
-            sixthObject_6.SetActive(false); //
-        }
-
-
-        if (seventhObject_7 != null)
-        {
-            Objectives.Add(new Objective(seventhObject_7, seventhDescription, seventhEmoji));
-            seventhObject_7.SetActive(false); //
-        }
-
-        if (eightObject_8 != null)
-        {
-            Objectives.Add(new Objective(eightObject_8, eightDescription, eightEmoji));
-            eightObject_8.SetActive(false); //
-        }
-
-        if (ninthObject_9 != null)
-        {
-            Objectives.Add(new Objective(ninthObject_9, ninthDescription, ninthEmoji));
-            ninthObject_9.SetActive(false); //
-        }
-
-        if (tenthObject_10 != null)
-        {
-            Objectives.Add(new Objective(tenthObject_10, tenthDescription, tenthEmoji));
-            tenthObject_10.SetActive(false); //
-        }*/
+        
 
         StartCoroutine(GameStartDelay(3));
-        //pauseDialogText.GetComponent<TextMeshProUGUI>().text = gameStartObjectiveDescription;
-        //ObjMarkerSingle.PlayAtObjective(Objectives[objectiveCount].ObjectiveObj);
-        //_phoneUI.SendEmojiMessage(Objectives[0].emoji);
         foreach (Message m in Objectives[0].messages)
         {
             _phoneUI.SetNotifyMessage(m);
@@ -188,7 +85,6 @@ public class ObjectivesManager : MonoBehaviour
     private void Update()
     {
         _sinceLastObj += Time.deltaTime;
-        
     }
    
     // public function when player triggers objective tag
@@ -208,10 +104,6 @@ public class ObjectivesManager : MonoBehaviour
             //_phoneUI.SetNotifyMessage(Objectives[objectiveCount].ObjectiveDes);
             if (objectiveCount < Objectives.Length)
             {
-                //GetComponent<TextBubble>().SpawnBubble(Objectives[objectiveCount].emoji);
-                //_phoneUI.SendEmojiMessage(Objectives[objectiveCount].emoji);
-                //pauseDialogText.GetComponent<TextMeshProUGUI>().text = Objectives[objectiveCount-1].messages[0].messageText;
-                //_phoneUI.SetNotifyMessage(Objectives[objectiveCount].ObjectiveDes.Substring(0, Objectives[objectiveCount].ObjectiveDes.Length - 2));
                 foreach (Message m in Objectives[objectiveCount].messages)
                 {
                     _phoneUI.SetNotifyMessage(m);
@@ -285,7 +177,6 @@ public class ObjectivesManager : MonoBehaviour
     {
         yield return new WaitForSeconds(4);
         end_of_level_event.Post(gameObject, (uint)(AkCallbackType.AK_EndOfEvent), GetComponent<GameController>().AdvanceLevel);
-
     }
 
 }
