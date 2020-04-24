73588ba5cefcfeba6f71d3af8d7b158b438cc31b
Tristan Bowler <tristan.bowler@gmail.com>
2020-04-24 02:45:21 -0600

    actually done for tonight

--- Personal_Space/Assets/Scripts/PatrolEnemyControllerStop.cs
+++ Personal_Space/Assets/Scripts/PatrolEnemyControllerStop.cs
@@ -1,49 +1,53 @@
-﻿ using UnityEngine;
-    using UnityEngine.AI;
-    using System.Collections;
+﻿using UnityEngine;
+using UnityEngine.AI;
+using System.Collections;
 
 
-    public class PatrolEnemyControllerStop : MonoBehaviour {
+public class PatrolEnemyControllerStop : MonoBehaviour
+{
 
-        public Transform[] points;
-        private int destPoint = 0;
-        private NavMeshAgent agent;
-        private float agentSpeed;
-        public float waitTime = 4f;
+    public Transform[] points;
+    private int destPoint = 0;
+    private NavMeshAgent agent;
+    private float agentSpeed;
+    public float waitTime = 4f;
 
-        private bool waiting = false;
+    private bool waiting = false;
     private GameController gameController;
 
-        private PlayerController playerController;
-        void Start () {
-            agent = GetComponent<NavMeshAgent>();
-            agentSpeed = agent.speed;
-            playerController = GameObject.FindGameObjectWithTag("Player").GetComponent<PlayerController>();
+    private PlayerController playerController;
+    void Start()
+    {
+        agent = GetComponent<NavMeshAgent>();
+        agentSpeed = agent.speed;
+        playerController = GameObject.FindGameObjectWithTag("Player").GetComponent<PlayerController>();
         gameController = playerController.gameObject.GetComponent<GameController>();
-            // Disabling auto-braking allows for continuous movement
-            // between points (ie, the agent doesn't slow down as it
-            // approaches a destination point).
-            agent.autoBraking = false;
+        // Disabling auto-braking allows for continuous movement
+        // between points (ie, the agent doesn't slow down as it
+        // approaches a destination point).
+        agent.autoBraking = false;
 
-            GotoNextPoint();
-        }
-
-
-        void GotoNextPoint() {
-            // Returns if no points have been set up
-            if (points.Length == 0)
-                return;
-
-            // Set the agent to go to the currently selected destination.
-            agent.destination = points[destPoint].position;
-
-            // Choose the next point in the array as the destination,
-            // cycling to the start if necessary.
-            destPoint = (destPoint + 1) % points.Length;
-        }
+        GotoNextPoint();
+    }
 
 
-        void Update () {
+    void GotoNextPoint()
+    {
+        // Returns if no points have been set up
+        if (points.Length == 0)
+            return;
+
+        // Set the agent to go to the currently selected destination.
+        agent.destination = points[destPoint].position;
+
+        // Choose the next point in the array as the destination,
+        // cycling to the start if necessary.
+        destPoint = (destPoint + 1) % points.Length;
+    }
+
+
+    void Update()
+    {
         // Choose the next destination point when the agent gets
         // close to the current one.
         if (gameController.isGamePaused())
@@ -52,29 +56,29 @@
             return;
         }
         if (!agent.pathPending && agent.remainingDistance < 0.5f)
+        {
+            if (!waiting)
             {
-                if(!waiting)
-                {
-                    agent.speed = 0;
-                    waiting = true;
-                    StartCoroutine(Loiter());
-                }
-            }
-
-            if(!waiting)
-            {
-                
-                    agent.speed = agentSpeed;
-                
-                
+                agent.speed = 0;
+                waiting = true;
+                StartCoroutine(Loiter());
             }
         }
 
-        private IEnumerator Loiter()
+        if (!waiting)
         {
-             yield return new WaitForSeconds(waitTime);
-             agent.speed = agentSpeed;
-             GotoNextPoint();
-             waiting = false;
+
+            agent.speed = agentSpeed;
+
+
         }
     }
+
+    private IEnumerator Loiter()
+    {
+        yield return new WaitForSeconds(waitTime);
+        agent.speed = agentSpeed;
+        GotoNextPoint();
+        waiting = false;
+    }
+}
